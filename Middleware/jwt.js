abe8f47ec9368de28dba53c1027e43fa978c8db3
spar.coder-at-151217770739 <L/SglWuRQGTDdMFmECCR9kdPQzEd7IYeTkIbEn6MaIg=>
2022-10-18 14:44:31 +0530

    second commit

--- Middleware/jwt.js
+++ Middleware/jwt.js
@@ -26,22 +26,4 @@ const validateToken = (req, res, next) => {
   }
 };
 
-const validateClient = (req, res, next) => {
-  const accessToken = req.cookie["emailToken"];
-  // console.log("Token " + accessToken);
-  if (!accessToken) {
-    res.sendStatus(403);
-  } else {
-    try {
-      const validToken = jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET);
-      req.token = validToken;
-      return next();
-    } catch (error) {
-      console.log("[jwt.js].validateToken catch error "+error);
-      res.sendStatus(403);;
-    }
-  }
-};
-
-
-module.exports = { createToken, validateToken, validateClient};
+module.exports = { createToken, validateToken};
